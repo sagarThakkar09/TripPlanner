99a4db2df17952c298444fe3e71f9fc532c38283
Sagar Thakkar <sthakkar42@gmail.com>
2020-05-22 11:20:53 -0500

    display origin location list into DOM

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,15 @@ function displayResult(query) {
     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${access_token}&limit=10&&bbox=-97.325875,49.766204,-96.953987,49.99275`)
         .then(resp => resp.json())
         .then(data => {
-
+            originlistEle.innerHTML = "";
+            locationsHTML = ""
+            data.features.forEach(location => {
+                console.log(location)
+                locationsHTML += `<li data-long="${location.center[0]}" data-lat="${location.center[1]}" class="selected">
+                  <div class="name">${location.text}</div>
+                  <div>${location.properties.address === undefined ? location.context[2].text:location.properties.address}</div>
+                </li>`
+            });
+            originlistEle.innerHTML = locationsHTML;
         })
 }
